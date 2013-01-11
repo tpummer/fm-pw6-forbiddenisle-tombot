0e97957512cfa16a39c99f0d802b7ba87c0512e6
Thomas Pummer <dev@nullpointer.at>
2013-01-11 18:21:18 +0100

    reads Gameboard

--- tombot.py
+++ tombot.py
@@ -5,6 +5,7 @@ import os, sys, string
 def main(argv):
 
     runningGame = True;
+    gameBoard = 1;
 
     while(runningGame):
         input = raw_input();
@@ -12,9 +13,9 @@ def main(argv):
         if( input == 'END'):
                 runningGame = False;
         elif(input.startswith('GAMEBOARDSTART')):
-             readGameBoard();
+             gameBoard = readGameBoard(input);
         elif(input.startswith('ROUND')):
-             print "bingo2";
+             #TODO make3Moves(gameBoard, input);
         elif(input.startswith('INCRFLOOD')):
              print "bingo3";
         elif(input.startswith('FLOOD')):
@@ -24,15 +25,32 @@ def main(argv):
 
     print "The End of the Game";
 
-def readGameBoard():
+def readGameBoard(startline):
     readBoard = True
 
+    indexOfSpace = startline.index(' ');
+    indexOfSemikolon = startline.index(',');
+
+    xGameBoard = int(startline[indexOfSpace+1:indexOfSemikolon])
+    yGameBoard = int(startline[indexOfSemikolon+1:])
+
+    gameBoard = [["." for i in range(int(xGameBoard))] for j in range(int(yGameBoard))]
+
+    #read y zeilen mit x zeichen
+    y = 0
     while(readBoard):
         input = raw_input();
+        
         if(input == 'GAMEBOARDEND'):
             readBoard = False;
-            
-    print "Done!";
+        else:
+            for x in range(0,xGameBoard):
+                gameBoard[x][y] = input[x:x+1]
+
+        y = y + 1
+
+    
+    return gameBoard
     
 
 if __name__ == "__main__":
